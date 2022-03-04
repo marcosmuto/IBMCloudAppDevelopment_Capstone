aa5e40d35539b623f9c673790c3b5009c8bfd9ed
marcos.muto <marcos.muto@gmail.com>
2022-03-04 16:20:41 +0000

    adding static pages

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -16,26 +16,35 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 
+def index(request):
+    context = {}
+    return render(request, 'djangoapp/index.html', context)
 
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
+def about(request):
+    context = {}
+    return render(request, 'djangoapp/about.html', context)
 
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
+def contact(request):
+    context = {}
+    return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    #logout(request)
+    return redirect('djangoapp:index')
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    #logout(request)
+    return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    #logout(request)
+    return redirect('djangoapp:index')
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
